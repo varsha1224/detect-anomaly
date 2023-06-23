10bfb1f00ada63345370466330f1fd59e7cee339
Varsha Subramonian <114343556+varsha1224@users.noreply.github.com>
2023-06-23 16:27:41 +0530

    Add files via upload

--- final_die_anomaly.py
+++ final_die_anomaly.py
@@ -1,7 +1,6 @@
+from PIL import Image
 import json
-import cv2
 import os
-import numpy as np
 import csv
 
 # importing the json file
@@ -15,79 +14,75 @@ with open (json_file) as f:
     streetWidth = data['street_width']
     careAreas = data['care_areas']
     exclusionZones = data.get('exclusion_zones')
-    
-'''
-print(dieWidth)    
-print(dieHeight)
-print(streetWidth)
-print(careAreas)
-'''
+
 
 # Take the wafer images and process it
 
 image_directory = "C:\\Users\\91978\\Desktop\\ANOMALY\\images_1"
 
-defects = []
-
-images = []
-subResult = 0
-i = 0
+colour_freq = {}
 
 # Iterate over the files in the image directory
 for filename in os.listdir(image_directory):
     if filename.endswith('.png') and filename.startswith('wafer_image_'):
         
         #img = cv2.imread()
-        # Extract the frame index from the file name
-        frameIndex = int(filename.split('_')[-1].split('.')[0])
+        # Extracting the die index from the file name
+        dieIndex = int(filename.split('_')[-1].split('.')[0])
         
         #print(frameIndex)
         
         # Load the image
         image_path = os.path.join(image_directory, filename)
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         
+        image = Image.open(image_path)
+        image = image.convert('RGB')
+        pixels = image.load()
+        
+        #setting care area coordinates
+        x1 = careAreas[0]['top_left']['x']
+        x2 = careAreas[0]['bottom_right']['x']
+        y1 = careAreas[0]['bottom_right']['y']
+        y2 = careAreas[0]['top_left']['y']
+
         # Cropping the images since we are concerned only about the care areas
-        image = image[0:800, 0:600]
-        
-        # Calculate the dimensions of the image
-        dimensions = image.shape
- 
-        # height, width
-        imageHeight = image.shape[0]
-        imageWidth = image.shape[1]
-        
-        # No. of image frames per die
-        n = (dieHeight * dieWidth) // (imageHeight * imageWidth)
-        
-        images.append(image)
-        
-        if len(images) == 5:
-            subResult = np.zeros_like(images[0]) # Initialize the subtraction result
-            
-            for i in range(1, len(images)):
-                subResult -= cv2.subtract(images[i], images[i-1])
-                
-            coords = np.argwhere(subResult > 0)
-            for coord in coords:
-                defects.append((frameIndex, coord[1], coord[0]))
-                
-            images.pop(0) # Remove the oldest image from the list
+        # Finding the colour that occurred the most times
+        for y in range(y1, y2):
+            for x in range(x1, x2):
+                colour = pixels[x, y]
+                if colour in colour_freq:
+                    colour_freq[colour] += 1
+                else:
+                    colour_freq[colour] = 1
 
-defectPoints = []
-# Print the defect information
-for defect in defects:
-    dieIndex, x, y = defect
-    #print("{0}, {1}, {2}".format(dieIndex, x, y))
-    
-    defectPoints.append((dieIndex, x, y))
-    
-with open("defect.csv","a+") as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerows(defectPoints)
-    
-    
-
-
+        colours = {} 
         
-        
+        # Background
+        maxcol1 = max(colour_freq.values())
+        for key in colour_freq:
+            if colour_freq[key] == maxcol1:
+                major_col1 = key
+        colours[major_col1] = colour_freq.pop(major_col1)
+    
+        # White lines
+        maxcol2 = max(colour_freq.values())
+        for key in colour_freq:
+            if colour_freq[key] == maxcol2:
+                major_col2 = key
+        colours[major_col2] = colour_freq.pop(major_col2)
+    
+        defectPoints = []
+        defect_xy = []
+    
+        for y in range(y1, y2):
+            for x in range(x1, x2):
+                colour = pixels[x, y]
+                if colour != major_col1 and colour != major_col2:
+                    defectPoints.append((dieIndex, x, y2-y-1))
+        #print(defectPoints)
+
+    # writing into file
+    with open("defect_2.csv","a+") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(defectPoints)
+
